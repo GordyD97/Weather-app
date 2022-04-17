b12ff37070359016c58111bd23e844587db2bf79
Turbo Gordz <gordon.draine97@yahoo.com>
2022-04-16 21:03:43 -0400

    adding html

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+
+// var apiurl = "https://api.openweathermap.org/data/2.5/forecast?lat=39.8693&lon=75.3824&appid=5ab9fa835693949a979219cf860d1300"
+
+
+
+// get correct api format
+//find a way to call api 
+
+  
+
+
+// function getApi(apiurl) {
+//   fetch(apiurl)
+//   .then(function (response) {
+//     console.log('hello')
+//   }
+//   )}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getApi() {
+//     // Insert the API url to get a list of your repos
+//     var weatherapi = weatherapi;
+  
+//     fetch(weatherapi)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         //looping over the fetch response and inserting the URL of your repos into a list
+//         for (var i = 0; i < data.length; i++) {
+//             //Create a list element
+//             var listItem = document.createElement('li');
+    
+//             //Set the text of the list element to the JSON response's .html_url property
+//             listItem.textContent = data[i].html_url;
+    
+//             //Append the li element to the id associated with the ul element.
+//             // repoList.appendChild(listItem);
+//         }
+//     });
+// }
